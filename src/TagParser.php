bf4cb380913970af5f2a699e8a50a83067dc681b
Hasan Tareque <hmtareque@gmail.com>
2015-01-02 12:19:50 +0000

    test with pipe separator

--- src/TagParser.php
+++ src/TagParser.php
@@ -4,6 +4,6 @@ namespace App;
 class TagParser {
     public function parse(String $tags) : array
     {
-        return preg_split("/, ?/", $tags);
+        return preg_split("/ ?[,|] ?/", $tags);
     }
 }
--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -26,11 +26,25 @@ class TagParserTest extends TestCase
         $expected = ['personal', 'family', 'money'];
 
         $this->assertSame($expected, $result);
+    }
+
+    public function test_it_parses_a_comma_separated_list_of_tags_without_space()
+    {
+        $parser = new TagParser;
 
         $result = $parser->parse("personal,family,money");
         $expected = ['personal', 'family', 'money'];
 
         $this->assertSame($expected, $result);
+    }
 
+    public function test_it_parses_a_pipe_separated_list()
+    {
+        $parser = new TagParser;
+
+        $result = $parser->parse("personal | family | money");
+        $expected = ['personal', 'family', 'money'];
+
+        $this->assertSame($expected, $result);
     }
 }
