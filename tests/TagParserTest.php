ef2e591c1a0d2d12e127e38e670de28c53cf5af9
Hasan Tareque <hmtareque@gmail.com>
2015-01-03 12:45:19 +0000

    added setUp method

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -7,25 +7,27 @@ use PHPUnit\Framework\TestCase;
 
 class TagParserTest extends TestCase
 {
+    protected $parser;
+
+    protected function setUp(): void
+    {
+        $this->parser = new TagParser();
+    }
+
     public function test_it_parses_a_single_tag()
     {
-        // Arrange 
-        $parser = new TagParser();
-
+        
         // Act
-        $result = $parser->parse("personal");
+        $result = $this->parser->parse("personal");
         $expected = ['personal'];
 
-        // Assert 
+        // Assert
         $this->assertSame($expected, $result);
-
     }
 
     public function test_it_parses_a_comma_separated_list_of_tags()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse("personal, family, money");
+        $result = $this->parser->parse("personal, family, money");
         $expected = ['personal', 'family', 'money'];
 
         $this->assertSame($expected, $result);
@@ -33,9 +35,7 @@ class TagParserTest extends TestCase
 
     public function test_it_parses_a_comma_separated_list_of_tags_without_space()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse("personal,family,money");
+        $result = $this->parser->parse("personal,family,money");
         $expected = ['personal', 'family', 'money'];
 
         $this->assertSame($expected, $result);
@@ -43,9 +43,7 @@ class TagParserTest extends TestCase
 
     public function test_it_parses_a_pipe_separated_list()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse("personal|family|money");
+        $result = $this->parser->parse("personal|family|money");
         $expected = ['personal', 'family', 'money'];
 
         $this->assertSame($expected, $result);
